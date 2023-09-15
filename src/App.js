f3b0bee7ea4ed06c0b276ecd27a938481e815dd7
Kyle Barry <kylesbarry@gmail.com>
2023-09-15 09:47:50 -0400

    set up routing and basic pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cancel from "./pages/Cancel";
+import Success from "./pages/Success";
+import Store from "./pages/Store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <NavbarComponent />
+      <Router>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </Router>
+    </Container>
+  );
 }
 
 export default App;
